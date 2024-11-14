0d656e25536a818d8ee4152e2e10776bc9360f06
LeScooot <jeronimus.ryan@gmail.com>
2024-11-14 13:42:07 -0500

    adding ros packages

---
+++ build/my_camera/share/my_camera/hook/pythonpath_develop.ps1
@@ -0,0 +1,3 @@
+# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
+
+colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/home/astrobotics/arcuo/build/my_camera"
